0922a04c613129782acf603dde8e07e6678da31d
gBarbieri <elect86@gmail.com>
2017-11-06 12:45:04 +0100

    glVertexAttribPointer int pointer

--- src/main/kotlin/gln/vertexArray/vertexArray.kt
+++ src/main/kotlin/gln/vertexArray/vertexArray.kt
@@ -1,6 +1,9 @@
 package gln.vertexArray
 
+import glm_.L
+import glm_.i
 import gln.get
+import org.lwjgl.opengl.GL20
 import org.lwjgl.opengl.GL30
 import java.nio.IntBuffer
 import kotlin.properties.Delegates
@@ -13,3 +16,6 @@ inline fun glBindVertexArray(vertexArray: Enum<*>) = GL30.glBindVertexArray(vert
 inline fun glBindVertexArray(vertexArray: IntBuffer) = GL30.glBindVertexArray(vertexArray[0])
 
 inline fun glBindVertexArray() = GL30.glBindVertexArray(0)
+
+inline fun glVertexAttribPointer(index: Int, size: Int, type: Int, normalized: Boolean, stride: Int, pointer: Int)
+        = GL20.glVertexAttribPointer(index, size, type, normalized, stride, pointer.L)
