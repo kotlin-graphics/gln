8796e79b0f1e124b17f80143487d1ec777513b27
gBarbieri <elect86@gmail.com>
2017-12-12 11:15:40 +0100

    viewport(width, height)

--- src/main/kotlin/gln/gln.kt
+++ src/main/kotlin/gln/gln.kt
@@ -23,41 +23,41 @@ operator fun IntArray.set(e: Enum<*>, int: Int) = set(e.ordinal, int)
 inline fun <reified T : Enum<T>> intArrayBig() = IntArray(enumValues<T>().size)
 
 
-fun glViewport(size: Vec2i) = GL11.glViewport(0, 0, size.x, size.y)
-fun glViewport(width: Int, height: Int) = GL11.glViewport(0, 0, width, height)
-fun glViewport(viewport: Vec4i) = GL11.glViewport(viewport.x, viewport.y, viewport.z, viewport.w)
+inline fun glViewport(size: Vec2i) = GL11.glViewport(0, 0, size.x, size.y)
+inline fun glViewport(width: Int, height: Int) = GL11.glViewport(0, 0, width, height)
+inline fun glViewport(viewport: Vec4i) = GL11.glViewport(viewport.x, viewport.y, viewport.z, viewport.w)
 
-fun glScissor(size: Vec2i) = GL11.glScissor(0, 0, size.x, size.y)
-fun glScissor(scissor: Vec4i) = GL11.glScissor(scissor.x, scissor.y, scissor.z, scissor.w)
+inline fun glScissor(size: Vec2i) = GL11.glScissor(0, 0, size.x, size.y)
+inline fun glScissor(scissor: Vec4i) = GL11.glScissor(scissor.x, scissor.y, scissor.z, scissor.w)
 
-fun glBlitFramebuffer(size: Vec2i) = GL30.glBlitFramebuffer(
+inline fun glBlitFramebuffer(size: Vec2i) = GL30.glBlitFramebuffer(
         0, 0, size.x, size.y,
         0, 0, size.x, size.y,
         GL11.GL_COLOR_BUFFER_BIT, GL11.GL_LINEAR)
 
 
-fun glClearColor() = GL11.glClearColor(0f, 0f, 0f, 1f)
-fun glClearColor(color: Vec3) = GL11.glClearColor(color.x, color.y, color.z, 1f)
-fun glClearColor(color: Vec4) = GL11.glClearColor(color.x, color.y, color.z, color.w)
-fun glClearDepthf() = GL41.glClearDepthf(1f)
+inline fun glClearColor() = GL11.glClearColor(0f, 0f, 0f, 1f)
+inline fun glClearColor(color: Vec3) = GL11.glClearColor(color.x, color.y, color.z, 1f)
+inline fun glClearColor(color: Vec4) = GL11.glClearColor(color.x, color.y, color.z, color.w)
+inline fun glClearDepthf() = GL41.glClearDepthf(1f)
 
 
-fun glGetVec2(pname: Int): Vec2 {
+inline fun glGetVec2(pname: Int): Vec2 {
     GL11.nglGetFloatv(pname, bufAd)
     return Vec2(buf)
 }
 
-fun glGetVec2i(pname: Int): Vec2i {
+inline fun glGetVec2i(pname: Int): Vec2i {
     GL11.nglGetIntegerv(pname, bufAd)
     return Vec2i(buf)
 }
 
-fun glGetVec4(pname: Int): Vec4 {
+inline fun glGetVec4(pname: Int): Vec4 {
     GL11.nglGetFloatv(pname, bufAd)
     return Vec4(buf)
 }
 
-fun glGetVec4i(pname: Int): Vec4i {
+inline fun glGetVec4i(pname: Int): Vec4i {
     GL11.nglGetIntegerv(pname, bufAd)
     return Vec4i(buf)
 }
