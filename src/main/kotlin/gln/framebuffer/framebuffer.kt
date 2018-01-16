f2b5d96f6d109f446cf77863b5ca70ab20a9d4e3
gBarbieri <elect86@gmail.com>
2018-01-16 17:00:25 +0100

    glFramebufferTexture

--- src/main/kotlin/gln/framebuffer/framebuffer.kt
+++ src/main/kotlin/gln/framebuffer/framebuffer.kt
@@ -4,6 +4,8 @@ package gln.framebuffer
 import gln.get
 import gln.renderbuffer.renderbufferName
 import org.lwjgl.opengl.GL30
+import org.lwjgl.opengl.GL30.GL_FRAMEBUFFER
+import org.lwjgl.opengl.GL32
 import java.nio.IntBuffer
 import kotlin.properties.Delegates
 
@@ -26,4 +28,5 @@ inline fun glBindFramebuffer(framebuffer: Int) = GL30.glBindFramebuffer(GL30.GL_
 inline fun glBindFramebuffer() = GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0)
 
 
-inline fun glFramebufferTexture2D(target: Int, attachment: Int, textarget: Int, texture: Int) = GL30.glFramebufferTexture2D(target, attachment, textarget, texture, 0)
+//inline fun glFramebufferTexture2D(target: Int, attachment: Int, textarget: Int, texture: Int) = GL30.glFramebufferTexture2D(target, attachment, textarget, texture, 0) TODO renable without target
+inline fun glFramebufferTexture(attachment: Int, texture: Int, level: Int = 0) = GL32.glFramebufferTexture(GL_FRAMEBUFFER, attachment, texture, level)
