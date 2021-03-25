fab0a26b10cd2afc3756317bc63b3987c6ed9ca4
Giuseppe Barbieri <elect86@gmail.com>
2021-03-25 19:41:20 +0100

    v0.5.2+22

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import kx.lwjglImplementation
 import kx.lwjglTestImplementation
 
 plugins {
-    val build = "0.7.0+71"
+    val build = "0.7.0+79"
     id("kx.kotlin.11") version build
     id("kx.lwjgl") version build
     id("kx.dokka") version build
@@ -13,7 +13,7 @@ plugins {
     java
 }
 
-version = "0.5.2+21"
+version = "0.5.2+22"
 
 dependencies {
 
