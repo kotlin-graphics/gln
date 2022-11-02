bc25789e7fcb480fe434fc727c46bacab5a3790d
Giuseppe Barbieri <elect86@gmail.com>
2022-11-02 15:00:28 +0100

    0.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,64 @@
-import kx.*
-import org.lwjgl.Lwjgl
-import org.lwjgl.Lwjgl.Module.*
+import magik.createGithubPublication
+import magik.github
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.lwjgl.lwjgl
+import org.lwjgl.lwjgl.Module.*
 
 plugins {
-    fun kx(vararg p: Pair<String, String>) = p.forEach { id("io.github.kotlin-graphics.${it.first}") version it.second }
-    kx("align" to "0.0.7",
-       "base" to "0.0.10",
-       "publish" to "0.0.6",
-       "utils" to "0.0.5")
-    id("org.lwjgl.plugin") version "0.0.20"
+    kotlin("jvm") version embeddedKotlinVersion
+    id("org.lwjgl.plugin") version "0.0.29"
+    id("elect86.magik") version "0.3.1"
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+    github("kotlin-graphics/mary")
 }
 
 dependencies {
 
     implementation(kotlin("reflect"))
+    implementation("kotlin.graphics:gli:0.8.3.0-18")
+    implementation("kotlin.graphics:glm:0.9.9.1-5")
+    implementation("kotlin.graphics:unsigned:3.3.31")
+    implementation("kotlin.graphics:kool:0.9.68")
 
-    implementation(unsigned, kool, glm, gli)
-
-    //    compile group: 'org.jetbrains.kotlin.kapt', name: 'org.jetbrains.kotlin.kapt.gradle.plugin', version: '1.3.0-rc-146'
-
-    Lwjgl {
+    lwjgl {
         implementation(glfw, jemalloc, openal, opengl, opengles, stb)
         testImplementation(opengl)
     }
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.1")
 }
+
+kotlin.jvmToolchain {
+    this as JavaToolchainSpec
+    languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+tasks {
+    withType<KotlinCompile<*>>().all {
+        kotlinOptions {
+            freeCompilerArgs += listOf("-opt-in=kotlin.RequiresOptIn")
+        }
+    }
+    withType<Test>().configureEach { useJUnitPlatform() }
+}
+
+publishing {
+    publications {
+        createGithubPublication {
+            from(components["java"])
+            suppressAllPomMetadataWarnings()
+        }
+    }
+    repositories {
+        github {
+            domain = "kotlin-graphics/mary"
+        }
+    }
+}
+
+java { withSourcesJar() }
